4401d163456c875af1d35c5f3ed2ce2ec255b373
fblaha <blahaf@gmail.com>
2017-04-19 22:51:00 +0200

    fixing build

--- providers/betfair-ng/src/main/java/cz/fb/manaus/betfair/rest/MarketCountAware.java
+++ providers/betfair-ng/src/main/java/cz/fb/manaus/betfair/rest/MarketCountAware.java
@@ -3,19 +3,21 @@ package cz.fb.manaus.betfair.rest;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
+
+import static java.util.stream.Collectors.partitioningBy;
 
 public interface MarketCountAware {
     int MARKET_LIMIT = 100;
 
     static <T extends MarketCountAware> List<List<T>> split(List<T> countAwareList) {
+        Map<Boolean, List<T>> smallMarkets = countAwareList.stream()
+                .collect(partitioningBy(count -> count.getMarketCount() <= MARKET_LIMIT));
+
         int sum = 0;
         List<List<T>> result = new LinkedList<>();
         List<T> current = new LinkedList<>();
-        List<T> smallMarkets = countAwareList.stream().
-                filter(cntAware -> cntAware.getMarketCount() <= MARKET_LIMIT).
-                collect(Collectors.toList());
-        for (T countAware : smallMarkets) {
+        for (T countAware : smallMarkets.get(true)) {
             if (sum + countAware.getMarketCount() > MARKET_LIMIT) {
                 result.add(current);
                 current = new LinkedList<>();
@@ -27,9 +29,9 @@ public interface MarketCountAware {
             }
         }
         if (!current.isEmpty()) result.add(current);
-        countAwareList.stream()
-                .filter(MarketCountAware::isOverLimit)
-                .forEach(countAware -> result.add(Collections.singletonList(countAware)));
+        for (T countAware : smallMarkets.get(false)) {
+            result.add(Collections.singletonList(countAware));
+        }
         return result;
     }
 
