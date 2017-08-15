ce8aaff13bb937413236069c97ba3289a500a77c
fblaha <blahaf@gmail.com>
2017-08-15 21:22:11 +0200

    bet url as header

--- mns-rest/src/main/java/cz/fb/manaus/rest/MarketPricesController.java
+++ mns-rest/src/main/java/cz/fb/manaus/rest/MarketPricesController.java
@@ -13,9 +13,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collections;
@@ -46,7 +46,7 @@ public class MarketPricesController {
 
     @RequestMapping(value = "/markets/{id}/prices", method = RequestMethod.POST)
     public ResponseEntity<?> pushMarketPrices(@PathVariable String id,
-                                              @RequestParam Optional<String> betUrl,
+                                              @RequestHeader("MNS_BET_URL") Optional<String> betUrl,
                                               @RequestBody MarketPrices marketPrices) {
         marketDao.get(id).ifPresent(marketPrices::setMarket);
         MarketSnapshot marketSnapshot = new MarketSnapshot(marketPrices, Collections.emptyList(), Optional.empty());
