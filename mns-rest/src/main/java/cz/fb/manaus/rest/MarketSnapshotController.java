b04bcd04989be0e6fe92145fbadbe0e2510ed97d
fblaha <blahaf@gmail.com>
2017-08-22 23:44:54 +0200

    snapshot controller - part2

--- mns-rest/src/main/java/cz/fb/manaus/rest/MarketSnapshotController.java
+++ mns-rest/src/main/java/cz/fb/manaus/rest/MarketSnapshotController.java
@@ -2,6 +2,7 @@ package cz.fb.manaus.rest;
 
 import cz.fb.manaus.core.dao.BetActionDao;
 import cz.fb.manaus.core.dao.MarketDao;
+import cz.fb.manaus.core.model.Bet;
 import cz.fb.manaus.core.model.MarketPrices;
 import cz.fb.manaus.core.model.MarketSnapshot;
 import cz.fb.manaus.reactor.betting.BetManager;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.OptionalLong;
 import java.util.Set;
@@ -34,13 +36,36 @@ public class MarketSnapshotController {
     @RequestMapping(value = "/markets/{id}/snapshot", method = RequestMethod.POST)
     public ResponseEntity<?> pushMarketSnapshot(@PathVariable String id,
                                                 @RequestHeader("MNS_BET_URL") Optional<String> betUrl,
-                                                @RequestBody MarketPrices marketPrices) {
+                                                @RequestBody MarketSnapshotCrate snapshotCrate) {
+        MarketPrices marketPrices = snapshotCrate.getPrices();
         marketDao.get(id).ifPresent(marketPrices::setMarket);
-        MarketSnapshot marketSnapshot = new MarketSnapshot(marketPrices, Collections.emptyList(), Optional.empty());
+        List<Bet> bets = Optional.ofNullable(snapshotCrate.getBets()).orElse(Collections.emptyList());
+        MarketSnapshot marketSnapshot = new MarketSnapshot(marketPrices, bets, Optional.empty());
         Set<String> myBets = actionDao.getBetActionIds(id, OptionalLong.empty(), Optional.empty());
         if (betUrl.isPresent()) {
             manager.silentFire(marketSnapshot, myBets, betUrl);
         }
         return ResponseEntity.accepted().build();
+    }
+}
+
+class MarketSnapshotCrate {
+    private MarketPrices prices;
+    private List<Bet> bets;
+
+    public MarketPrices getPrices() {
+        return prices;
+    }
+
+    public void setPrices(MarketPrices prices) {
+        this.prices = prices;
+    }
+
+    public List<Bet> getBets() {
+        return bets;
+    }
+
+    public void setBets(List<Bet> bets) {
+        this.bets = bets;
     }
 }
--- mns-rest/src/test/java/cz/fb/manaus/rest/MarketSnapshotControllerTest.java
+++ mns-rest/src/test/java/cz/fb/manaus/rest/MarketSnapshotControllerTest.java
@@ -1,9 +1,17 @@
 package cz.fb.manaus.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.fb.manaus.core.model.Bet;
+import cz.fb.manaus.core.model.MarketPrices;
+import cz.fb.manaus.core.model.Price;
+import cz.fb.manaus.core.model.Side;
+import cz.fb.manaus.core.test.CoreTestFactory;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Collections;
+import java.util.Date;
 
 import static cz.fb.manaus.core.test.CoreTestFactory.newMarketPrices;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -16,9 +24,15 @@ public class MarketSnapshotControllerTest extends AbstractControllerTest {
     @Test
     public void testPushSnapshot() throws Exception {
         createMarketWithSingleAction();
-        String prices = new ObjectMapper().writer().writeValueAsString(newMarketPrices(3, 2.8d));
+        MarketPrices marketPrices = newMarketPrices(3, 2.8d);
+        MarketSnapshotCrate crate = new MarketSnapshotCrate();
+        crate.setPrices(marketPrices);
+        Bet bet = new Bet("1", marketPrices.getMarket().getId(), CoreTestFactory.DRAW,
+                new Price(3d, 5d, Side.BACK), new Date(), 0d);
+        crate.setBets(Collections.singletonList(bet));
+        String snapshot = new ObjectMapper().writer().writeValueAsString(crate);
         mvc.perform(post("/markets/{id}/snapshot", MARKET_ID)
-                .content(prices)
+                .content(snapshot)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isAccepted())
                 .andReturn();
