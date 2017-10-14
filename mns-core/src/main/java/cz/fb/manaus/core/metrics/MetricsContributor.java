3770f611db684dad240badc8e484c6c95db5a5dd
fblaha <blahaf@gmail.com>
2017-10-15 01:51:46 +0200

    prefix

--- mns-core/src/main/java/cz/fb/manaus/core/metrics/MetricsContributor.java
+++ mns-core/src/main/java/cz/fb/manaus/core/metrics/MetricsContributor.java
@@ -10,17 +10,18 @@ public interface MetricsContributor {
 
     default Stream<MetricRecord<?>> getMeterMetricRecords(String name, MetricRegistry metricRegistry) {
         Meter meter = metricRegistry.meter(name);
+        String prefix = "meter." + name;
         return Stream.of(
-                new MetricRecord<>(name + ".count", meter.getCount()),
-                new MetricRecord<>(name + ".rate15", meter.getFifteenMinuteRate()),
-                new MetricRecord<>(name + ".rate5", meter.getFiveMinuteRate()),
-                new MetricRecord<>(name + ".rate1", meter.getOneMinuteRate()),
-                new MetricRecord<>(name + ".meanRate", meter.getMeanRate()));
+                new MetricRecord<>(prefix + ".count", meter.getCount()),
+                new MetricRecord<>(prefix + ".rate15", meter.getFifteenMinuteRate()),
+                new MetricRecord<>(prefix + ".rate5", meter.getFiveMinuteRate()),
+                new MetricRecord<>(prefix + ".rate1", meter.getOneMinuteRate()),
+                new MetricRecord<>(prefix + ".meanRate", meter.getMeanRate()));
     }
 
     default Stream<MetricRecord<?>> getCounterMetricRecords(String name, MetricRegistry metricRegistry) {
         Counter counter = metricRegistry.counter(name);
-        return Stream.of(new MetricRecord<>(name, counter.getCount()));
+        return Stream.of(new MetricRecord<>("counter." + name, counter.getCount()));
     }
 
     Stream<MetricRecord<?>> getMetricRecords();
