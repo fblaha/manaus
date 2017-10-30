25c384d3f2349c03d1c54e9f7179589faa5be194
fblaha <blahaf@gmail.com>
2017-10-30 21:36:58 +0100

    setting metrics over rest

--- mns-rest/src/main/java/cz/fb/manaus/rest/MetricsController.java
+++ mns-rest/src/main/java/cz/fb/manaus/rest/MetricsController.java
@@ -1,13 +1,17 @@
 package cz.fb.manaus.rest;
 
+import com.codahale.metrics.Histogram;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.SlidingWindowReservoir;
 import cz.fb.manaus.core.metrics.MetricRecord;
 import cz.fb.manaus.core.metrics.MetricsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -31,5 +35,24 @@ public class MetricsController {
     public List<MetricRecord<?>> getMetrics(@PathVariable String prefix) {
         metricRegistry.counter("metric.get").inc();
         return metricsService.getCollectedMetrics(prefix);
+    }
+
+    @RequestMapping(value = "/metrics/histogram/{name}", method = RequestMethod.POST)
+    public void updateHistogram(@PathVariable String name,
+                                @RequestParam(defaultValue = "100") int reservoirSize,
+                                @RequestBody long value) {
+        metricRegistry.histogram(name,
+                () -> new Histogram(new SlidingWindowReservoir(reservoirSize)))
+                .update(value);
+    }
+
+    @RequestMapping(value = "/metrics/meter/{name}", method = RequestMethod.POST)
+    public void updateMeter(@PathVariable String name, @RequestBody long value) {
+        metricRegistry.meter(name).mark(value);
+    }
+
+    @RequestMapping(value = "/metrics/counter/{name}", method = RequestMethod.POST)
+    public void updateCounter(@PathVariable String name, @RequestBody long value) {
+        metricRegistry.counter(name).inc(value);
     }
 }
--- mns-rest/src/test/java/cz/fb/manaus/rest/MetricsControllerTest.java
+++ mns-rest/src/test/java/cz/fb/manaus/rest/MetricsControllerTest.java
@@ -1,14 +1,58 @@
 package cz.fb.manaus.rest;
 
+import com.codahale.metrics.MetricRegistry;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @ContextConfiguration(classes = MetricsController.class)
 public class MetricsControllerTest extends AbstractControllerTest {
 
+    @Autowired
+    private MetricRegistry metricRegistry;
+
     @Test
     public void testMetrics() throws Exception {
         checkResponse("/metrics", "metric.get", "1");
+    }
+
+    @Test
+    public void testUpdateHistogram() throws Exception {
+        mvc.perform(post(
+                "/metrics/histogram/{name}", "histogram.test")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("100"))
+                .andExpect(status().isOk())
+                .andReturn();
+        assertThat(metricRegistry.histogram("histogram.test").getCount(), is(1L));
+    }
+
+    @Test
+    public void testUpdateMeter() throws Exception {
+        mvc.perform(post(
+                "/metrics/meter/{name}", "meter.test")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("100"))
+                .andExpect(status().isOk())
+                .andReturn();
+        assertThat(metricRegistry.meter("meter.test").getCount(), is(100L));
+    }
+
+    @Test
+    public void testUpdateCounter() throws Exception {
+        mvc.perform(post(
+                "/metrics/counter/{name}", "counter.test")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("100"))
+                .andExpect(status().isOk())
+                .andReturn();
+        assertThat(metricRegistry.counter("counter.test").getCount(), is(100L));
     }
 }
