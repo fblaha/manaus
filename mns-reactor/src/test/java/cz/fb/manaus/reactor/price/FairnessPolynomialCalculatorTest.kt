acdc4b1bab62a230934101917fb177d80881b011
fblaha <blahaf@gmail.com>
2018-08-19 23:31:33 +0200

    kotlin intro - tests (p5)

---
+++ mns-reactor/src/test/java/cz/fb/manaus/reactor/price/FairnessPolynomialCalculatorTest.kt
@@ -0,0 +1,69 @@
+package cz.fb.manaus.reactor.price
+
+import cz.fb.manaus.core.test.AbstractLocalTestCase
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.*
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.*
+
+
+class FairnessPolynomialCalculatorTest : AbstractLocalTestCase() {
+
+
+    @Autowired
+    private lateinit var calculator: FairnessPolynomialCalculator
+
+    private fun toOptional(values: List<Double>): List<OptionalDouble> {
+        return values.map { OptionalDouble.of(it) }
+    }
+
+    @Test
+    fun `polynomial fairness`() {
+        assertEquals(0.866, calculator.getFairness(1.0, toOptional(listOf(2.5, 1.5))).asDouble, 0.001)
+        assertEquals(0.825, calculator.getFairness(1.0, toOptional(listOf(2.7, 1.4))).asDouble, 0.001)
+        assertEquals(0.75, calculator.getFairness(1.0, toOptional(listOf(2.5, 2.5, 2.5))).asDouble, 0.001)
+        assertEquals(0.85, calculator.getFairness(1.0, toOptional(listOf(2.7, 2.7, 2.7))).asDouble, 0.001)
+    }
+
+    @Test
+    fun `fairness - complex case`() {
+        val fairness = calculator.getFairness(1.0, toOptional(BEST_PRICES_HARD)).asDouble
+        assertTrue(fairness > 0)
+    }
+
+    @Test
+    fun `fairness 1 winner`() {
+        assertThat(calculator.getFairness(1.0, toOptional(listOf(3.0, 3.0, 3.0))).asDouble, `is`(1.0))
+    }
+
+    @Test
+    fun `fairness 2 winners`() {
+        assertEquals(1.0, calculator.getFairness(2.0,
+                toOptional(listOf(1.5, 1.5, 1.5))).asDouble, 0.0001)
+    }
+
+    @Test
+    fun `fairness 2 winners - comparison`() {
+        assertTrue(calculator.getFairness(2.0, toOptional(listOf(1.4, 1.5, 1.5))).asDouble <
+                calculator.getFairness(2.0, toOptional(listOf(1.5, 1.5, 1.5))).asDouble)
+    }
+
+    @Test
+    fun `fairness lay 2 runners`() {
+        assertEquals(1.118, calculator.getFairness(1.0, toOptional(listOf(1.5, 3.5))).asDouble, 0.001)
+    }
+
+    @Test
+    fun `fairness lay 3 runners`() {
+        assertEquals(1.093, calculator.getFairness(1.0, toOptional(listOf(3.5, 3.5, 2.7))).asDouble, 0.001)
+    }
+
+    companion object {
+        val BEST_PRICES_HARD = listOf(
+                85.0, 510.0, 270.0, 700.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0,
+                1000.0, 1000.0, 1000.0, 1000.0, 38.0, 95.0, 110.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0,
+                1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0)
+    }
+
+}
---
+++ mns-reactor/src/test/java/cz/fb/manaus/reactor/price/PriceBulldozerTest.kt
@@ -0,0 +1,83 @@
+package cz.fb.manaus.reactor.price
+
+import cz.fb.manaus.core.model.Price
+import cz.fb.manaus.core.model.PriceComparator
+import cz.fb.manaus.core.model.Side
+import cz.fb.manaus.core.model.TradedVolume
+import cz.fb.manaus.core.test.AbstractLocalTestCase
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class PriceBulldozerTest : AbstractLocalTestCase() {
+
+    @Autowired
+    private lateinit var bulldozer: PriceBulldozer
+
+    private val realSample = listOf(
+            Price(6.0, 2.0, Side.BACK),
+            Price(3.0, 4.0, Side.BACK),
+            Price(2.8, 10.0, Side.BACK))
+
+    @Test
+    fun `bulldoze lay prices`() {
+        checkResult(3.0, listOf(Price(3.0, 2.0, Side.LAY), Price(4.0, 2.0, Side.LAY)), 1, 3.5, 4.0)
+        val three = listOf(Price(3.0, 2.0, Side.LAY),
+                Price(4.0, 2.0, Side.LAY), Price(5.0, 2.0, Side.LAY))
+        checkResult(3.0, three, 2, 3.5, 4.0)
+        checkResult(5.0, three, 1, 4.0, 6.0)
+    }
+
+    @Test
+    fun `bulldoze - only 1 price`() {
+        checkResult(3.0, listOf(Price(3.0, 2.0, Side.LAY)), 1, 3.0, 2.0)
+        checkResult(1.0, listOf(Price(3.0, 2.0, Side.LAY)), 1, 3.0, 2.0)
+    }
+
+    @Test
+    fun `threshold equal to amount sum`() {
+        val two = listOf(Price(4.0, 2.0, Side.BACK), Price(3.0, 2.0, Side.BACK))
+        val three = listOf(Price(5.0, 2.0, Side.BACK), Price(4.0, 2.0, Side.BACK),
+                Price(3.0, 2.0, Side.BACK))
+
+        checkResult(2.0, two, 2, 4.0, 2.0)
+        checkResult(4.0, three, 2, 4.5, 4.0)
+    }
+
+    @Test
+    fun `bulldoze real data`() {
+        checkResult(1.0, realSample, 3, 6.0, 2.0)
+        checkResult(2.0, realSample, 3, 6.0, 2.0)
+        checkResult(3.0, realSample, 2, 4.0, 6.0)
+        checkResult(4.0, realSample, 2, 4.0, 6.0)
+        checkResult(5.0, realSample, 2, 4.0, 6.0)
+        checkResult(6.0, realSample, 2, 4.0, 6.0)
+        checkResult(50.0, realSample, 1, 3.25, 16.0)
+        checkResult(100.0, realSample, 1, 3.25, 16.0)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `bulldozed prices are in wrong order - back`() {
+        val badOrder = PriceComparator.ORDERING.reverse<Price>().sortedCopy<Price>(realSample)
+        bulldozer.bulldoze(10.0, badOrder)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `bulldozed prices are in wrong order - lay`() {
+        bulldozer.bulldoze(10.0, listOf(Price(5.0, 2.0, Side.LAY), Price(4.0, 2.0, Side.LAY)))
+    }
+
+    private fun checkResult(threshold: Double, prices: List<Price>, expectedCount: Int,
+                            expectedPrice: Double, expectedAmount: Double) {
+        val bulldozed = bulldozer.bulldoze(threshold, prices)
+        assertEquals(expectedCount, bulldozed.size)
+        assertEquals(expectedPrice, bulldozed[0].price, 0.0001)
+        assertEquals(expectedAmount, bulldozed[0].amount, 0.0001)
+
+        assertEquals(prices.map { it.amount }.sum(),
+                bulldozed.map { it.amount }.sum(), 0.0001)
+        assertEquals(TradedVolume.getWeightedMean(prices).asDouble,
+                TradedVolume.getWeightedMean(bulldozed).asDouble, 0.0001)
+    }
+
+}
