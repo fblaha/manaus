6ff04836ae6d948ab61ae014df559b71c65a9539
fblaha <blahaf@gmail.com>
2018-08-25 20:08:46 +0200

    kotlin intro - tests (p35) - final

--- mns-rest/src/test/java/cz/fb/manaus/rest/IntervalParserTest.kt
+++ mns-rest/src/test/java/cz/fb/manaus/rest/IntervalParserTest.kt
@@ -23,14 +23,14 @@ class IntervalParserTest : AbstractLocalTestCase() {
     @Test
     fun `offset lower endpoint`() {
         val now = Instant.now()
-        assertEquals(now.minus(6, ChronoUnit.HOURS), intervalParser!!.parse(now, "2h-4").lowerEndpoint())
+        assertEquals(now.minus(6, ChronoUnit.HOURS), intervalParser.parse(now, "2h-4").lowerEndpoint())
         assertEquals(now.minus(12, ChronoUnit.DAYS), intervalParser.parse(now, "10d-2").lowerEndpoint())
     }
 
     @Test
     fun `offset upper endpoint`() {
         val now = Instant.now()
-        assertEquals(now.minus(4, ChronoUnit.HOURS), intervalParser!!.parse(now, "2h-4").upperEndpoint())
+        assertEquals(now.minus(4, ChronoUnit.HOURS), intervalParser.parse(now, "2h-4").upperEndpoint())
         assertEquals(now.minus(2, ChronoUnit.DAYS), intervalParser.parse(now, "10d-2").upperEndpoint())
     }
 }
