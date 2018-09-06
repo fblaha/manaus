1646e90696555a526ca2fe80e958b1a465f1bb60
fblaha <blahaf@gmail.com>
2018-09-06 23:06:55 +0200

    kotlin

---
+++ mns-rest/src/main/java/cz/fb/manaus/rest/MatchedBetMetricUpdater.kt
@@ -0,0 +1,38 @@
+package cz.fb.manaus.rest
+
+import com.codahale.metrics.Histogram
+import com.codahale.metrics.MetricRegistry
+import com.codahale.metrics.SlidingWindowReservoir
+import cz.fb.manaus.core.model.Bet
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import java.util.concurrent.atomic.AtomicLong
+
+@Component
+class MatchedBetMetricUpdater {
+    private val lastScan = AtomicLong(0)
+
+    @Autowired
+    private lateinit var metricRegistry: MetricRegistry
+
+    private fun newHistogram(): Histogram {
+        return Histogram(SlidingWindowReservoir(100))
+    }
+
+    fun update(scanTime: Long, bets: List<Bet>) {
+        val last = lastScan.getAndSet(scanTime)
+        if (last > 0 && last != scanTime) {
+            val lastCount = metricRegistry.counter(METRIC_NAME).count
+            metricRegistry.remove(METRIC_NAME)
+            metricRegistry.histogram("bet.matched", MetricRegistry.MetricSupplier<Histogram> { this.newHistogram() }).update(lastCount)
+        }
+        val currentCount = bets.filter { it.isHalfMatched }.count()
+        if (currentCount > 0) {
+            metricRegistry.counter(METRIC_NAME).inc(currentCount.toLong())
+        }
+    }
+
+    companion object {
+        const val METRIC_NAME = "bet.matched.count"
+    }
+}
