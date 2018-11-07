49c5f58819e9f471c9a77cfc82c7bc44b6f8c49d
fblaha <blahaf@gmail.com>
2018-11-07 20:13:41 +0100

    market repo

--- mns-repository/src/main/java/cz/fb/manaus/repository/MarketRepository.kt
+++ mns-repository/src/main/java/cz/fb/manaus/repository/MarketRepository.kt
@@ -1,11 +1,13 @@
 package cz.fb.manaus.repository
 
 import cz.fb.manaus.core.persistence.Market
-import org.dizitart.kno2.filters.eq
-import org.dizitart.kno2.filters.lt
+import org.dizitart.kno2.filters.*
 import org.dizitart.kno2.getRepository
+import org.dizitart.no2.FindOptions
 import org.dizitart.no2.Nitrite
+import org.dizitart.no2.SortOrder
 import org.dizitart.no2.objects.ObjectRepository
+import org.dizitart.no2.objects.filters.ObjectFilters
 import java.time.Instant
 
 
@@ -24,4 +26,24 @@ class MarketRepository(private val db: Nitrite) {
     fun deleteMarkets(olderThan: Instant): Int {
         return repository.remove(Market::openDate lt olderThan).affectedCount
     }
+
+    fun delete(id: String) {
+        repository.remove(Market::id eq id)
+    }
+
+    fun getMarkets(from: Instant?, to: Instant?, maxResults: Int?): List<Market> {
+        var filter = ObjectFilters.ALL
+        if (from != null) {
+            val fromFilter = Market::openDate gte from
+            filter = filter?.and(fromFilter) ?: fromFilter
+        }
+        if (to != null) {
+            val toFilter = Market::openDate lte to
+            filter = filter?.and(toFilter) ?: toFilter
+        }
+        var options = FindOptions.sort("openDate", SortOrder.Ascending)
+        if (maxResults != null) options = options.thenLimit(0, maxResults)
+        return repository.find(filter, options).toList()
+    }
+
 }
--- mns-repository/src/test/java/cz/fb/manaus/repository/MarketRepositoryTest.kt
+++ mns-repository/src/test/java/cz/fb/manaus/repository/MarketRepositoryTest.kt
@@ -41,10 +41,18 @@ class MarketRepositoryTest {
     }
 
     @Test
-    fun `save and read`() {
+    fun `save - read`() {
         marketRepository.save(market)
         val fromDB = marketRepository.read("2")
         assertEquals(market, fromDB)
+    }
+
+    @Test
+    fun `save - delete - read`() {
+        marketRepository.save(market)
+        assertNotNull(marketRepository.read("2"))
+        marketRepository.delete("2")
+        assertNull(marketRepository.read("2"))
     }
 
     @Test
@@ -54,5 +62,41 @@ class MarketRepositoryTest {
         assertNotNull(marketRepository.read("2"))
         assertEquals(1, marketRepository.deleteMarkets(Instant.now().plusSeconds(10)))
         assertNull(marketRepository.read("2"))
+    }
+
+    @Test
+    fun `get markets from`() {
+        marketRepository.save(market)
+        assertEquals(1, marketRepository.getMarkets(null, null, null).size)
+        assertEquals(0, marketRepository.getMarkets(Instant.now().plusSeconds(30), null, null).size)
+        assertEquals(1, marketRepository.getMarkets(Instant.now().minusSeconds(30), null, null).size)
+    }
+
+    @Test
+    fun `get markets to`() {
+        marketRepository.save(market)
+        val minus30 = Instant.now().minusSeconds(30)
+        val plus30 = Instant.now().plusSeconds(30)
+        assertEquals(1, marketRepository.getMarkets(minus30, plus30, null).size)
+        assertEquals(0, marketRepository.getMarkets(minus30, minus30, null).size)
+    }
+
+    @Test
+    fun `get markets limit`() {
+        marketRepository.save(market)
+        marketRepository.save(market.copy(id = "3"))
+        assertEquals(2, marketRepository.getMarkets(null, null, 2).size)
+        assertEquals(1, marketRepository.getMarkets(null, null, 1).size)
+    }
+
+    @Test
+    fun `get markets sort`() {
+        val laterEvent = market.event.copy(openDate = Instant.now().plusSeconds(30))
+        marketRepository.save(market.copy(id = "3", event = laterEvent))
+        marketRepository.save(market)
+        val markets = marketRepository.getMarkets(null, null, null)
+        assertEquals(2, markets.size)
+        assertEquals("2", markets.first().id)
+        assertEquals("3", markets.last().id)
     }
 }
