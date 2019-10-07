c7c819f00ede4f453592845f70690f911f04a6a8
fblaha <blahaf@gmail.com>
2019-10-08 01:45:39 +0300

    lay proposer fix

--- ischia/src/main/java/cz/fb/manaus/ischia/proposer/FairnessLayProposer.kt
+++ ischia/src/main/java/cz/fb/manaus/ischia/proposer/FairnessLayProposer.kt
@@ -1,13 +1,13 @@
 package cz.fb.manaus.ischia.proposer
 
 import cz.fb.manaus.core.model.Side
-import cz.fb.manaus.ischia.BackLoserBet
 import cz.fb.manaus.ischia.LayLoserBet
 import cz.fb.manaus.reactor.betting.BetContext
 import cz.fb.manaus.reactor.betting.proposer.common.AbstractFairnessProposer
 import org.springframework.stereotype.Component
 
 @Component
-@BackLoserBet
+// TODO enable for back
+//@BackLoserBet
 @LayLoserBet
-class FairnessLayProposer(downgradeStrategy: (BetContext) -> Double) : AbstractFairnessProposer(Side.BACK, downgradeStrategy)
+class FairnessLayProposer(downgradeStrategy: (BetContext) -> Double) : AbstractFairnessProposer(Side.LAY, downgradeStrategy)
