add544715661687d32dbb6095dd5dec2fdcc32be
fblaha <blahaf@gmail.com>
2019-10-25 21:33:38 +0300

    provider tests

---
+++ mns-core/src/test/java/cz/fb/manaus/core/provider/ExchangeProviderTest.kt
@@ -0,0 +1,32 @@
+package cz.fb.manaus.core.provider
+
+import cz.fb.manaus.core.model.provider
+import cz.fb.manaus.core.provider.ProviderTag.*
+import org.junit.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+private class MockSelector(override val tags: Set<ProviderTag>) : ProviderSelector
+
+class ExchangeProviderTest {
+
+    @Test
+    fun validate() {
+        provider.validate()
+    }
+
+    @Test
+    fun `invalid tags`() {
+        val invalid = provider.copy(tags = setOf(PriceShiftContinuous, PriceShiftFixedStep))
+        assertFailsWith<IllegalStateException> { invalid.validate() }
+    }
+
+    @Test
+    fun `match tags`() {
+        assertFalse { provider.matches(MockSelector(setOf(ProviderMatchbook))) }
+        assertTrue { provider.matches(MockSelector(setOf(ProviderBetfair))) }
+        assertTrue { provider.matches(MockSelector(setOf(ProviderBetfair, PriceShiftFixedStep))) }
+        assertFalse { provider.matches(MockSelector(setOf(ProviderBetfair, PriceShiftContinuous))) }
+    }
+}
