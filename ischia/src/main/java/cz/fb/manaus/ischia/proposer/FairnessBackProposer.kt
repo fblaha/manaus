1732503c33dd1191a61dc6fb8f9b740ba7c66e3b
fblaha <blahaf@gmail.com>
2019-10-31 01:57:29 +0300

    back fairness proposer - lay bets

--- ischia/src/main/java/cz/fb/manaus/ischia/proposer/FairnessBackProposer.kt
+++ ischia/src/main/java/cz/fb/manaus/ischia/proposer/FairnessBackProposer.kt
@@ -2,10 +2,12 @@ package cz.fb.manaus.ischia.proposer
 
 import cz.fb.manaus.core.model.Side
 import cz.fb.manaus.ischia.BackLoserBet
+import cz.fb.manaus.ischia.LayLoserBet
 import cz.fb.manaus.reactor.betting.proposer.DowngradeStrategy
 import cz.fb.manaus.reactor.betting.proposer.common.AbstractFairnessProposer
 import org.springframework.stereotype.Component
 
 @Component
+@LayLoserBet
 @BackLoserBet
 class FairnessBackProposer(vararg downgradeStrategy: DowngradeStrategy) : AbstractFairnessProposer(Side.BACK, *downgradeStrategy)
