1193562a5064ddd457c5113de09c8198dac49263
fblaha <blahaf@gmail.com>
2019-11-12 01:10:02 +0300

    active selection - test

--- mns-core/src/test/java/cz/fb/manaus/core/model/MarketSnapshotTest.kt
+++ mns-core/src/test/java/cz/fb/manaus/core/model/MarketSnapshotTest.kt
@@ -4,6 +4,8 @@ import org.junit.Test
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class MarketSnapshotTest {
 
@@ -20,6 +22,18 @@ class MarketSnapshotTest {
         val coverage = getMarketCoverage(listOf(successor, predecessor))
         assertEquals(1, coverage.size)
         assertEquals(successor, coverage[SideSelection(side, selectionId)])
+    }
+
+    @Test
+    fun `is active`() {
+        val bet = Bet(marketId = "1",
+                selectionId = 1L,
+                requestedPrice = Price(2.0, 2.0, Side.LAY),
+                placedDate = Instant.now().minus(2, ChronoUnit.HOURS),
+                matchedAmount = 1.0)
+        val coverage = getMarketCoverage(listOf(bet))
+        assertTrue { coverage.isActive(bet.selectionId) }
+        assertFalse { coverage.isActive(bet.selectionId + 100) }
     }
 
 }
