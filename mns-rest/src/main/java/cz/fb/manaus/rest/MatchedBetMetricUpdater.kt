00f8247c35f12510f1ede6486d3cc1c99c60f351
fblaha <blahaf@gmail.com>
2020-01-21 02:22:42 +0300

    covered metric

--- mns-rest/src/main/java/cz/fb/manaus/rest/MatchedBetMetricUpdater.kt
+++ mns-rest/src/main/java/cz/fb/manaus/rest/MatchedBetMetricUpdater.kt
@@ -11,6 +11,9 @@ import java.util.concurrent.atomic.AtomicLong
 class MatchedBetMetricUpdater {
     private val lastScan = AtomicLong(0)
 
+    private val coveredBets: AtomicLong by lazy {
+        Metrics.gauge("mns_bet_covered_count", AtomicLong())
+    }
     private val matchedLayBets: AtomicLong by lazy {
         Metrics.gauge("mns_bet_matched_count", listOf(Tag.of("side", "lay")), AtomicLong())
     }
@@ -27,6 +30,11 @@ class MatchedBetMetricUpdater {
         val matched = bets.filter { it.isHalfMatched }
         update(matched.count { it.requestedPrice.side == Side.BACK }, matchedBackBets)
         update(matched.count { it.requestedPrice.side == Side.LAY }, matchedLayBets)
+        val covered = 2 * matched
+                .groupBy { it.marketId to it.selectionId }.values
+                .map { it.distinctBy { bet -> bet.requestedPrice.side }.size }
+                .count { it == 2 }
+        update(covered, coveredBets)
     }
 
     private fun update(count: Int, counter: AtomicLong) {
