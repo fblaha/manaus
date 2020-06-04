3bb836ca1b0baee98eb1a6e729d2a3a3a2948992
fblaha <blahaf@gmail.com>
2020-06-05 02:55:41 +0300

    all matched

---
+++ ischia/src/main/java/cz/fb/manaus/ischia/validator/AllRunnersMatchedValidator.kt
@@ -0,0 +1,22 @@
+package cz.fb.manaus.ischia.validator
+
+import cz.fb.manaus.ischia.BackUniverse
+import cz.fb.manaus.ischia.LayUniverse
+import cz.fb.manaus.reactor.betting.BetEvent
+import cz.fb.manaus.reactor.betting.validator.ValidationResult
+import cz.fb.manaus.reactor.betting.validator.Validator
+import org.springframework.stereotype.Component
+
+@BackUniverse
+@LayUniverse
+@Component
+object AllRunnersMatchedValidator : Validator {
+
+    // TODO use OPA
+    override fun validate(event: BetEvent): ValidationResult {
+        val prices = event.marketPrices
+        if (prices.size > 2) return ValidationResult.OK
+        return if (prices.mapNotNull { it.matchedAmount }.all { it > 0.0 }) ValidationResult.OK else ValidationResult.NOP
+    }
+
+}
