e9b0718932eebd52838f2ce4b5d8aed69459f6ba
fblaha <blahaf@gmail.com>
2020-06-09 21:13:03 +0200

    match odds

--- mns-reactor/src/main/java/cz/fb/manaus/reactor/categorizer/MatchOddsCategorizer.kt
+++ mns-reactor/src/main/java/cz/fb/manaus/reactor/categorizer/MatchOddsCategorizer.kt
@@ -2,14 +2,22 @@ package cz.fb.manaus.reactor.categorizer
 
 import cz.fb.manaus.core.category.categorizer.RealizedBetCategorizer
 import cz.fb.manaus.core.model.RealizedBet
+import cz.fb.manaus.core.model.TYPE_MATCH_ODDS
 import org.springframework.stereotype.Component
 
 @Component
-class SelectionOrderCategorizer : RealizedBetCategorizer {
+class MatchOddsCategorizer : RealizedBetCategorizer {
 
     override fun getCategories(realizedBet: RealizedBet): Set<String> {
         val market = realizedBet.market
+        if (market.type != TYPE_MATCH_ODDS) return emptySet()
         val runner = market.runners.first { it.selectionId == realizedBet.settledBet.selectionId }
-        return setOf("selectionOrder_" + runner.sortPriority)
+        val runnerName = when (runner.sortPriority) {
+            0 -> "home"
+            1 -> "away"
+            2 -> "draw"
+            else -> "undefined"
+        }
+        return setOf("matchOdds_$runnerName")
     }
 }
