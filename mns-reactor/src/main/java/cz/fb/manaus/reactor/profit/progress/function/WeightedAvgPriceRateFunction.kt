d8d03899491865ff24066d88efa1942d083ac685
Filip Blaha <blahaf@gmail.com>
2021-01-24 12:45:20 +0100

    weighted avg

--- mns-reactor/src/main/java/cz/fb/manaus/reactor/profit/progress/function/WeightedAvgPriceRateFunction.kt
+++ mns-reactor/src/main/java/cz/fb/manaus/reactor/profit/progress/function/WeightedAvgPriceRateFunction.kt
@@ -1,6 +1,7 @@
 package cz.fb.manaus.reactor.profit.progress.function
 
 import cz.fb.manaus.core.model.RealizedBet
+import cz.fb.manaus.core.model.Side
 import cz.fb.manaus.reactor.price.getRunnerPrices
 import cz.fb.manaus.reactor.price.getWeightedMean
 import org.springframework.stereotype.Component
@@ -11,10 +12,14 @@ object WeightedAvgPriceRateFunction : ProgressFunction {
     override fun invoke(bet: RealizedBet): Double? {
         val runnerPrices = bet.betAction.runnerPrices
         val settledBet = bet.settledBet
+        val side = settledBet.price.side
         val prices = getRunnerPrices(runnerPrices, settledBet.selectionId)
-                .getHomogeneous(settledBet.price.side)
+                .getHomogeneous(side)
         val price = settledBet.price.price
         val avg = getWeightedMean(prices.prices) ?: error("empty")
-        return if (avg > price) avg / price else price / avg
+        return when (side) {
+            Side.BACK -> price / avg
+            Side.LAY -> avg / price
+        }
     }
 }
