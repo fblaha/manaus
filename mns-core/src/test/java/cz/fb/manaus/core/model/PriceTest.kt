1fbbc8c27d32494aeabe86db2accbf06937c04ff
Filip Blaha <blahaf@gmail.com>
2021-06-06 21:38:59 +0200

    param tests

--- mns-core/src/test/java/cz/fb/manaus/core/model/PriceTest.kt
+++ mns-core/src/test/java/cz/fb/manaus/core/model/PriceTest.kt
@@ -1,6 +1,8 @@
 package cz.fb.manaus.core.model
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
 
@@ -11,4 +13,28 @@ class PriceTest {
         assertEquals(Price(2.28, 2.24, Side.LAY), Price(2.28, 2.24, Side.LAY))
         assertNotEquals(Price(2.28, 2.04, Side.LAY), Price(2.28, 2.24, Side.LAY))
     }
+
+    @ParameterizedTest
+    @CsvSource(
+        "2, 2",
+        "1.99999999, 2",
+        "3.0000001, 3",
+        "3.01, 3.01"
+    )
+    fun round(input: Double, expected: Double) {
+        assertEquals(expected, Price.round(input))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "2, 2, true",
+        "2, 1.99999999, true",
+        "2, 2.00000001, true",
+        "2, 2.1, false",
+        "2, 3, false",
+    )
+    fun priceEq(p1: Double, p2: Double, expected: Boolean) {
+        assertEquals(expected, p1 priceEq p2)
+    }
+
 }
